b4986a6555756d21cca33d5a35a42d9aa3c528c3
mjoss17 <matthewjoss17@gmail.com>
2022-10-04 16:40:02 -0500

    Fix export-users uuid efficiency

--- moodlecli/main.py
+++ moodlecli/main.py
@@ -252,7 +252,7 @@ def export_users(source_course_id, bucket_name, key):
     user_data = moodle.get_users_by_course(
         source_course_id
     )
-    uuid_data = moodle.get_user_uuids([])
+    uuid_data = moodle.get_user_uuids([source_course_id])
     user_data = utils.inject_uuids(uuid_data, user_data)
 
     aws.put_json_data(user_data, bucket_name, key)
